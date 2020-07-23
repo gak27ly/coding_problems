0bca2cee40a84a6a4670645edaedc2e2f95a6cca
Yuan Li <liyu4@oregonstate.edu>
2020-07-22 23:36:57 -0700

    binary search

--- binarySearch.py
+++ binarySearch.py
@@ -129,10 +129,97 @@ def peakIndexInMountainArray(self, A):
         return start
     else:
         return end
+'''
+Suppose a sorted array in ascending order is rotated at 
+some pivot unknown to you beforehand.
+No duplicate exists
+解法： 找到中间位置和结尾比较大小 
+'''      
+
+def findMin(self, nums):
+    # write your code here
+    if not nums:
+        return None
+    start, end = 0, len(nums) - 1
+    
+    while start + 1 < end:
+        mid = (start + end) // 2
         
+        if nums[mid] > nums[end]:
+            start = mid
+        else:
+            end = mid
         
+    if nums[start] < nums[end]:
+        return nums[start]
+    return nums[end]
+
+class Solution:
+    """
+    @param A: an integer rotated sorted array
+    @param target: an integer to be searched
+    @return: an integer
+    解法： 注重有序的那部分
+    注意：需要把等于的部分考虑进去，否则会出错
+    """
+def search(self, A, target):
+    # write your code here
+    if not A:
+        return -1
+    start, end = 0, len(A) - 1
+    
+    while start + 1 < end:
+        mid = (start + end) // 2
         
-        
-        
+        if A[mid] < A[end]:
+            if A[mid] <= target <= A[end]:
+                start = mid
+            else:
+                end = mid
+        else:
+            if A[start] <= target <= A[mid]:
+                end = mid
+            else:
+                start = mid
+    
+    if A[start] == target:
+        return start
+    if A[end] == target:
+        return end
+    
+    return -1
+
+'''
+            
+There is an integer array which has the following features:
+
+The numbers in adjacent positions are different.
+A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
+We define a position P is a peak if:
+
+A[P] > A[P-1] && A[P] > A[P+1]
+Find a peak element in this array. Return the index of the peak.
+
+It's guaranteed the array has at least one peak.
+The array may contain multiple peeks, find any of them.
+The array has at least 3 numbers in it.
+解法： 找到中间位置 和左右相比，哪边大就保留哪边，如果中间大那就是peak.
+'''
+def findPeak(self, A):
+# write your code here
+    start, end = 1, len(A) - 2
+    while start + 1 <  end:
+        mid = (start + end) // 2
+        if A[mid] < A[mid - 1]:
+            end = mid
+        elif A[mid] < A[mid + 1]:
+            start = mid
+        else:
+            return mid
+
+    if A[start] < A[end]:
+        return end
+    else:
+        return start
             
 
