6e00044c911d31db29d4a79ddf26f0d1ec9e1d5c
Joseph_Li <32281157+gak27ly@users.noreply.github.com>
2020-07-28 23:06:12 -0700

    dfs

--- dfs.py
+++ dfs.py
@@ -83,4 +83,113 @@ def dfs(s, decode, res):
             
 print(numDecodings("105"))
 
+'''
+135. Combination Sum
+Given a set of candidtate numbers candidates and a target number target. 
+Find all unique combinations in candidates where the numbers sums to target.
+The same repeated number may be chosen from candidates unlimited number of times.
+注意点： 去重后，使用startIndex来防止再次传入之前的数字
+'''
+def combinationSum(self, candidates, target):
+    # write your code here
+    
+    if not candidates:
+        return [[]]
+    newcandidates = sorted(list(set(candidates)))
+    
+    res = [] 
+    self.helper(newcandidates, 0, target, [], res)
+    return res
 
+def helper(self, candidates, startIndex, target, combination, res):
+    if target == 0:
+        res.append(list(combination))
+        return
+    if target < 0:
+        return 
+    
+    for i in range(startIndex, len(candidates)):
+        combination.append(candidates[i])
+        self.helper(candidates, i, target - candidates[i], combination, res)
+        combination.pop()
+'''
+153. Combination Sum II
+中文English
+Given an array num and a number target. 
+Find all unique combinations in num where the numbers sum to target.
+
+Each number in num can only be used once in one combination.
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+注意点： 不能重复使用统一数字，需要从i + 1开始找
+'''
+def combinationSum2(self, nums, target):
+    # write your code here
+    if not nums:
+        return []
+    newNums = sorted(nums)
+    res = []
+    
+    self.helper(newNums, 0, target, [], res)
+    return res
+
+def helper(self, nums, startIndex, target, combination, res):
+    if target == 0:
+        res.append(list(combination))
+    
+    if target < 0:
+        return
+    lastItem = None
+    
+    for i in range(startIndex, len(nums)):
+        if nums[i] > target:
+            continue
+        if lastItem == nums[i]:
+            continue
+        combination.append(nums[i])
+        self.helper(nums, i + 1, target - nums[i], combination, res)
+        lastItem = combination.pop()
+'''
+10. String Permutation II
+Given a string, find all permutations of it without duplicates.
+注意点： 字符串需要先排序，否则无法去重
+Example
+
+Input: "aabb"
+Output:
+["aabb", "abab", "baba", "bbaa", "abba", "baab"]
+'''
+def stringPermutation2(self, str):
+    # write your code here
+    if not str:
+        return [""]
+        
+    visited = [False for _ in range(len(str))]
+    
+    str =sorted(list(str))
+    
+    res = []
+    
+    self.helper(str, visited, [], res)
+    
+    return res
+
+def helper(self, str, visited, premutation, res):
+    if len(premutation) == len(str):
+        res.append("".join(premutation))
+        return
+    
+    lastChar = None
+    
+    for i in range(len(str)):
+        if visited[i]:
+            continue
+        
+        if lastChar == str[i]:
+            continue
+        
+        premutation.append(str[i])
+        visited[i] = True
+        self.helper(str, visited, premutation, res)
+        lastChar = premutation.pop()
+        visited[i] = False  
