86e7894c72326ee6fa22620bd764804ba86d6a5b
Joseph_Li <32281157+gak27ly@users.noreply.github.com>
2020-09-11 22:11:26 -0700

    leetcode2

--- leetcode200.py
+++ leetcode200.py
@@ -46,3 +46,60 @@ def bfs(self, root, res):
                 queue.append(node.right)
             n -= 1
         res.append(level)
+
+
+def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+    if not root:
+        return []
+    res =[]
+    self.bfs(root, res)
+    return res
+
+def bfs(self, root, res):
+    queue = collections.deque([root])
+    direction = 1
+    while queue:
+        n = len(queue)
+        level= []
+        while n:
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            n -= 1
+        if direction == 1:
+            res.append(level)
+        else:
+            res.append(level[::-1])
+        direction = -direction
+
+'''
+方法二：
+通过调整元素append进入的位置来调整，避免了使用level[::-1]（时间复杂度为O(n)）
+'''
+
+    def bfs(self, root, res):
+        queue = collections.deque([root])
+        direction = True
+        while queue:
+            n = len(queue)
+            level= []
+            while n:
+                if direction:
+                    node = queue.popleft()
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+                else:
+                    node = queue.pop()
+                    if node.right:
+                        queue.appendleft(node.right)
+                    if node.left:
+                        queue.appendleft(node.left)
+                level.append(node.val)
+                n -= 1
+            res.append(level)
+            direction = not direction
